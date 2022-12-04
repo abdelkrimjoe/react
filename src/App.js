d927c57d2b5772440b181b951c59cd592a14fe21
abdelkrimjoe <abdelkrimgacemchaouch@gmail.com>
2022-12-04 14:29:32 +0100

    React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <section>
+      <h1 className="tittle">My FIrst</h1>
+      <h2>Hey</h2>
+    </section>
+
+  );
+}
+
+export default App;
